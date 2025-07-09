3899fe8dfc656b8b6298b02631c81d1a1f48c6c5
星月なるこ <projectcsj@gmail.com>
2025-07-09 09:56:47 +0800

    feat(ping): implement ping command with latency response and localization support

--- src/interactions/utils/ping.ts
+++ src/interactions/utils/ping.ts
@@ -1,17 +1,40 @@
-// TODO: Copy me to interactions/subfolder/your-command.ts
 import {
   SlashCommandBuilder,
+  EmbedBuilder,
   type ChatInputCommandInteraction,
 } from "discord.js"
 
-const commandName = {
+const ping = {
   data: new SlashCommandBuilder()
-    .setName("command-name")
-    .setDescription("command-description"),
+    .setName("ping")
+    .setNameLocalizations({
+      "zh-TW": "ping",
+      "zh-CN": "ping",
+      ja: "ping",
+    })
+    .setDescription("Get latency and API ping")
+    .setDescriptionLocalizations({
+      "zh-TW": "取得延遲和 API Ping",
+      "zh-CN": "获取延迟和 API Ping",
+      ja: "レイテンシとAPIのPingを取得",
+    }),
   async execute(interaction: ChatInputCommandInteraction) {
-    // ? any thing you want to do before reply here
-    await interaction.reply("content")
+    const embed = new EmbedBuilder()
+      .setColor("Random")
+      .setTitle("Pong!")
+      .setAuthor({
+        name: interaction.client.user.displayName,
+        iconURL: interaction.client.user.displayAvatarURL(),
+      })
+      .setDescription(
+        `WebSocket Ping: ${Math.round(interaction.client.ws.ping)}ms\n` +
+          `API Ping: ${Date.now() - interaction.createdTimestamp}ms`,
+      )
+      .setFooter({
+        text: `© ${new Date().getFullYear()} なるこStudio`,
+        iconURL: "https://naruko.studio/logo.svg",
+      })
+    await interaction.reply({ embeds: [embed] })
   },
 }
-
-export default commandName
+export default ping
